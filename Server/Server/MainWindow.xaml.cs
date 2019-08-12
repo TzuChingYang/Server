e5b3c7be4d44f89513f735179d7c23ea945a6fae
YangTzChing <rsrs6543210@gmail.com>
2019-08-13 00:03:36 +0800

    Something need to be modified

---
+++ Server/Server/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Server
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public string data = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Title_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+        private void Button_start_Click(object sender, RoutedEventArgs e)
+        {
+            StartListening();
+        }
+
+        public  void StartListening()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.  
+            // Dns.GetHostName returns the name of the   
+            // host running the application.  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP socket.  
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and   
+            // listen for incoming connections.  
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+                
+                // Start listening for connections.  
+                while (true)
+                {
+                    Textbox_output.Text="Waiting for a connection...";
+                    // Program is suspended while waiting for an incoming connection.  
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    // An incoming connection needs to be processed.  
+                    while (true)
+                    {
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Show the data on the console.  
+                    Textbox_output.Text="Text received : {0}"+ data;
+
+                    // Echo the data back to the client.  
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Textbox_output.Text=e.ToString();
+            }
+
+        }
+        public void Textbox_output_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        /* ===================================================== */
+        // About Socket server
+        //
+        /* ===================================================== */
+        // State object for reading client data asynchronously 
+        
+
+    }
+
+
+}
