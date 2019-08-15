4ed43a16d5cce992068d26047407f23486146afc
YangTzChing <rsrs6543210@gmail.com>
2019-08-15 20:50:18 +0800

    Socket HW done

--- Server/Server/MainWindow.xaml.cs
+++ Server/Server/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading;
 
 using System.Collections.Generic;
 using System.Linq;
@@ -26,10 +26,10 @@ namespace Server
     /// 
     public partial class MainWindow : Window
     {
-        public string data = null;
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void Title_TextChanged(object sender, TextChangedEventArgs e)
@@ -40,79 +40,54 @@ namespace Server
 
         private void Button_start_Click(object sender, RoutedEventArgs e)
         {
-            StartListening();
+            connect(Textbox_output);
         }
 
-        public  void StartListening()
-        {
-            // Data buffer for incoming data.  
-            byte[] bytes = new Byte[1024];
-
-            // Establish the local endpoint for the socket.  
-            // Dns.GetHostName returns the name of the   
-            // host running the application.  
-            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
-
-            // Create a TCP/IP socket.  
-            Socket listener = new Socket(ipAddress.AddressFamily,
-                SocketType.Stream, ProtocolType.Tcp);
-
-            // Bind the socket to the local endpoint and   
-            // listen for incoming connections.  
-            try
-            {
-                listener.Bind(localEndPoint);
-                listener.Listen(10);
-                
-                // Start listening for connections.  
-                while (true)
-                {
-                    Textbox_output.Text="Waiting for a connection...";
-                    // Program is suspended while waiting for an incoming connection.  
-                    Socket handler = listener.Accept();
-                    data = null;
-
-                    // An incoming connection needs to be processed.  
-                    while (true)
-                    {
-                        int bytesRec = handler.Receive(bytes);
-                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                        if (data.IndexOf("<EOF>") > -1)
-                        {
-                            break;
-                        }
-                    }
-
-                    // Show the data on the console.  
-                    Textbox_output.Text="Text received : {0}"+ data;
-
-                    // Echo the data back to the client.  
-                    byte[] msg = Encoding.ASCII.GetBytes(data);
-
-                    handler.Send(msg);
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
-                }
-
-            }
-            catch (Exception e)
-            {
-                Textbox_output.Text=e.ToString();
-            }
-
-        }
+       
         public void Textbox_output_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            
         }
         /* ===================================================== */
         // About Socket server
         //
         /* ===================================================== */
         // State object for reading client data asynchronously 
-        
+
+        static void connect(TextBox t)
+        {
+            int port = 9527;
+            string host = "10.211.55.3";
+            ///創建終結點（EndPoint）
+            IPAddress ip = IPAddress.Parse(host);//把IP地址字符串轉換為IPAddress類型的實例
+            IPEndPoint ipe = new IPEndPoint(ip, port);//用指定的端口和ip初始化IPEndPoint類的新實例 
+                                                      ///創建socket並開始監聽
+                                                      ///
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//創建一個socket對像，如果用udp協議，則要用SocketType.Dgram類型的套接字
+            s.Bind(ipe);//綁定EndPoint對像（2000端口和ip地址）
+            s.Listen(0);//開始監聽
+            t.Text = "Waiting for connect... \n";
+            Console.WriteLine("等待客戶端連接");
+            ///接受到client連接，為此連接建立新的socket，並接受信息
+            Socket temp = s.Accept();//為新建連接創建新的socket
+            t.AppendText(" Connect success \n");
+            Console.WriteLine("建立連接");
+            string recvStr = "";
+            byte[] recvBytes = new byte[1024];
+            int bytes;
+            bytes = temp.Receive(recvBytes, recvBytes.Length, 0);//從客戶端接受信息
+            recvStr += Encoding.ASCII.GetString(recvBytes, 0, bytes);
+            ///給client端返回信息
+            ///
+            Console.WriteLine("server get message:{0}", recvStr);//把客戶端傳來的信息顯示出來
+            t.AppendText("Server get message: "+recvStr+"\n");
+            string sendStr = "Server get message:"+recvStr;
+            byte[] bs = Encoding.ASCII.GetBytes(sendStr);
+            temp.Send(bs, bs.Length, 0);//返回信息給客戶端
+            temp.Close();
+            s.Close();
+            Console.ReadLine();
+        }
 
     }
 
